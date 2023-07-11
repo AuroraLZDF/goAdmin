e19743efdfff6b78330f2c8282a13463868d6606
aurora <18862324237@163.com>
2023-07-11 14:42:33 +0800

    Revert "feat：路由实现"
    This reverts commit a075fadd4e01d9feaad6f07e60464c04043a7906.

--- internal/apis/router.go
+++ internal/apis/router.go
@@ -6,14 +6,10 @@
 package apis
 
 import (
-	"github.com/gin-gonic/gin"
-
-	"apis/internal/apis/controller/v1/admin/auth"
-	"apis/internal/apis/store"
 	"apis/internal/pkg/core"
 	"apis/internal/pkg/errno"
-	"apis/internal/pkg/middleware"
 	"apis/pkg/log"
+	"github.com/gin-gonic/gin"
 )
 
 // installRouters 安装 apis 接口路由.
@@ -27,30 +23,30 @@ func installRouters(g *gin.Engine) error {
 	g.GET("/healthz", func(c *gin.Context) {
 		log.C(c).Info("Healthz function called")
 
-		core.Success(c, nil, "ok")
+		core.Success(c, gin.H{"status": "ok"}, "ok")
 	})
 
-	ac := auth.New(store.S)
+	//uc := user.New(store.S)
+	//pc := post.New(store.S)
+
+	//g.POST("/login", uc.Login)
 
 	// 创建 v1 路由分组
-	v1 := g.Group("/v1")
-	{
-		admin := v1.Group("/admin")
-		{
-			_auth := admin.Group("/auth")
-			{
-				_auth.POST("/login", ac.Login)
-
-				//创建 users 路由分组
-				user := _auth.Use(middleware.Authn())
-				{
-					user.GET("userInfo", ac.UserInfo)
-					user.POST("refresh", ac.RefreshToken)
-					user.POST("logout", ac.Logout)
-				}
-			}
-		}
-	}
+	//v1 := g.Group("/v1")
+	//{
+	//	//创建 users 路由分组
+	//	userv1 := v1.Group("/users").Use(middleware.Authn())
+	//	{
+	//		userv1.GET("info", uc.Detail)
+	//		userv1.POST("create", uc.Create)
+	//		userv1.PUT("change-password", uc.ChangePassword)
+	//		userv1.POST("logout", uc.Logout)
+	//		userv1.PUT(":name", uc.Update)    // 更新用户
+	//		userv1.GET("", uc.List)           // 列出用户列表，只有 root 用户才能访问
+	//		userv1.DELETE(":name", uc.Delete) // 删除用户
+	//	}
+	//
+	//}
 
 	return nil
 }
